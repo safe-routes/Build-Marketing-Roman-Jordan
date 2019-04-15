b1bcadf999982ca5f239a8a9a236caf6c9b78487
roman-jordan <rjordan1352@gmail.com>
2019-04-15 13:31:20 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Safe Routes | How Safe are you where you go?</title>
+    <meta name="description" content="How safe are you and your loved ones on your day to day adventure? At Safe Routes, we hope to make it so every trip will bring you home safe and sound">
+    
+</head>
+<body>
+    <header>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    <header>
+    
+    <section class="intro-content">
+        <div class="intro">
+            <h1>Making it Home has Never Been Easier</h1>
+            <p>Have you ever had a family member go to the store or ballet only to never return?
+            You ask your self WHY! WHAT COULD I HAVE DONE! HOW COULD THIS HAPPEN! At Safe Routes,
+            we don't want this to happen to any one ever again. Accidents do happen, however, when 
+            accidents happen in a given area there is now a trend established; A mark on the map if you will.
+            What we have done is taken all of these marks and hooked them up to Deep Learning, an A.I. 
+            program, to help uncover some of these accident trends  and patterns in an effort to help
+            families and people choose the safest, most efficient route possible. Safety Saves Lives!
+        </div>
+        <div class="intro-img">
+
+        </div>
+    
+    </section>
+    
+    <footer>
+
+    </footer>
+
+</body>
+</html>
+
