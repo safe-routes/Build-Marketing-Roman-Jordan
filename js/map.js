99bb77786ff7fee3dfdf7a272719744eea736acf
roman-jordan <rjordan1352@gmail.com>
2019-04-15 14:38:24 -0700

    Structured Less Files Set up with vars From Swatch Fan

--- js/map.js
+++ js/map.js
@@ -1,12 +1,11 @@
-<script>
-    // Initialize and add the map
+
+    
     function initMap() {
       // The location of Uluru
-      var uluru = {lat: -25.344, lng: 131.036};
+      var uluru = {lat: 37.81885833, lng: -122.18675833};
       // The map, centered at Uluru
       var map = new google.maps.Map(
           document.getElementById('map'), {zoom: 4, center: uluru});
       // The marker, positioned at Uluru
       var marker = new google.maps.Marker({position: uluru, map: map});
     }
-</script>
