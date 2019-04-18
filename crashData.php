bd83e5a3f02a07f5b932b8ddd62bc1de9d84dba0
roman-jordan <rjordan1352@gmail.com>
2019-04-18 15:58:50 -0700

    Routing issue solved, back to styling.

--- crashData.php
+++ crashData.php
@@ -6,9 +6,18 @@ header("Access-Control-Allow-Origin: *");//I allow all servers to request
 header("Access-Control-Allow-Headers: *");//I allow all headers
 
 if(isset($_POST)){
-    $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/".$_POST;//I pull from ED CHIN
+
+    $keys = "";
+    foreach($_REQUEST as $key=>$value){
+        $keys = $key;
+    }
+
+    $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/".$keys;//I pull from ED CHIN
     $crashData = file_get_contents($url);
     echo $crashData;
+    
+    
+
 }else if(isset($_GET)){
     $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/".$_GET;
     $crashData = file_get_contents($url);
