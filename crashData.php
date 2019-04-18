1a2f978b665916afa43b19a48aacee5f8de66501
roman-jordan <rjordan1352@gmail.com>
2019-04-18 09:11:31 -0700

    Desktop Navigation complete

--- crashData.php
+++ crashData.php
@@ -6,13 +6,16 @@ header("Access-Control-Allow-Origin: *");//I allow all servers to request
 header("Access-Control-Allow-Headers: *");//I allow all headers
 
 if(isset($_POST)){
-    $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/ALAMEDA";//I pull from ED CHIN
+    $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/".$_POST;//I pull from ED CHIN
     $crashData = file_get_contents($url);
     echo $crashData;
 }else if(isset($_GET)){
-    $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/ALAMEDA";
+    $url = "http://crashpredictr-env.jjrxtdfaz3.us-east-2.elasticbeanstalk.com/predict/".$_GET;
     $crashData = file_get_contents($url);
     echo $crashData;
 }else{
     return 'Post not set';
 }
+
+
+
